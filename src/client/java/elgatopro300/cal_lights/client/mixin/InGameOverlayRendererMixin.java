2615f0693cc41c1baa90c69bb54187e28eda1731
ElGatoPro300 <catgamer207@gmail.com>
2026-06-25 16:18:59 -0600

    fix crash

--- src/client/java/elgatopro300/cal_lights/client/mixin/InGameOverlayRendererMixin.java
+++ src/client/java/elgatopro300/cal_lights/client/mixin/InGameOverlayRendererMixin.java
@@ -4,6 +4,7 @@ import elgatopro300.cal_lights.ui.CALEditorScreen;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.InGameOverlayRenderer;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.texture.Sprite;
 import net.minecraft.client.util.math.MatrixStack;
 
@@ -15,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(InGameOverlayRenderer.class)
 public class InGameOverlayRendererMixin {
     @Inject(method = "renderInWallOverlay", at = @At("HEAD"), cancellable = true)
-    private static void cal_cancelInWallOverlay(Sprite sprite, MatrixStack matrices, CallbackInfo ci) {
+    private static void cal_cancelInWallOverlay(Sprite sprite, MatrixStack matrices, VertexConsumerProvider vertexConsumers, CallbackInfo ci) {
         if (MinecraftClient.getInstance().currentScreen instanceof CALEditorScreen) {
             ci.cancel();
         }
